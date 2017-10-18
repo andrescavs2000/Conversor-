d5b60b2e6e42435a05589ab26bd653d3b6106cac
Administrador <Administrador@wolf>
2017-10-17 22:58:49 -0500

    pude¡¡¡ XD

---
+++ src/convertidor/ConversorConcreto.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package convertidor;
+
+/**
+ *
+ * @author Administrador
+ */
+public class ConversorConcreto extends AConversor{
+    
+    @Override
+    double grado2radian(){
+        return (dato*Math.PI)/180;
+    }
+    
+    @Override
+    double farenheit2kelvin(){
+        return (dato+459.67)*5/9;
+        
+    }
+}
